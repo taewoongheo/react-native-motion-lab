580989815e7fdb0dfb331dc3aef309ee211885cb
taewoongheo <chamjoeun0111@gmail.com>
2025-04-06 14:24:36 +0900

    chore: set up react-native-nevigator

--- App.tsx
+++ App.tsx
@@ -15,7 +15,6 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import {
   Colors,
@@ -74,42 +73,38 @@ function App(): React.JSX.Element {
   const safePadding = '5%';
 
   return (
-    // <Modal {...rest}>
-    <GestureHandlerRootView>
-      <View style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView style={backgroundStyle}>
-          <View style={{paddingRight: safePadding}}>
-            <Header />
-          </View>
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-              paddingHorizontal: safePadding,
-              paddingBottom: safePadding,
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </View>
-    </GestureHandlerRootView>
-    // </Modal>
+    <View style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView style={backgroundStyle}>
+        <View style={{paddingRight: safePadding}}>
+          <Header />
+        </View>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            paddingHorizontal: safePadding,
+            paddingBottom: safePadding,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
--- android/app/src/main/java/com/interactionlab/MainActivity.kt
+++ android/app/src/main/java/com/interactionlab/MainActivity.kt
@@ -1,3 +1,4 @@
+import android.os.Bundle;
 package com.interactionlab
 
 import com.facebook.react.ReactActivity
@@ -6,6 +7,10 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
