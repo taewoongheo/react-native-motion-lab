3082d1aa04c2478f905d672a01415cac98c6d70b
taewoongheo <chamjoeun0111@gmail.com>
2025-04-07 18:26:27 +0900

    fix: avoid layout overlapping with status bar on Android

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Main from './src/main';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 import {colors} from './src/constants/colors';
 import RippleEffect from './src/interactions/RippleEffect';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 export type RootStackParamList = {
   Main: undefined;
@@ -17,27 +18,32 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Main"
-            screenOptions={{
-              headerShown: false,
-            }}>
-            <Stack.Screen
-              name="Main"
-              component={Main}
-              options={{title: 'Interaction Lab'}}
-            />
-            <Stack.Screen
-              name="RippleEffect"
-              component={RippleEffect}
-              options={{title: 'Ripple Effect'}}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+      <SafeAreaProvider style={styles.container}>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor={colors.background}
+        />
+        <SafeAreaView style={styles.container}>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Main"
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Screen
+                name="Main"
+                component={Main}
+                options={{title: 'Interaction Lab'}}
+              />
+              <Stack.Screen
+                name="RippleEffect"
+                component={RippleEffect}
+                options={{title: 'Ripple Effect'}}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
