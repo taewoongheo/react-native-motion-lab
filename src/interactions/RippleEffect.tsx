e8269f41a1f7107d2815dc3bdb586f8098c33566
taewoongheo <chamjoeun0111@gmail.com>
2025-04-07 21:13:44 +0900

    chore: set up react-native-skia
    Installed @shopify/react-native-skia@2.0.0-next.2 to support React Native 0.78.2 and React 19.0.0.
    Version was pinned explicitly to avoid unexpected updates from the `next` tag.

--- src/interactions/RippleEffect.tsx
+++ src/interactions/RippleEffect.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {theme} from '../constants/theme';
+import {Canvas, Circle, Group} from '@shopify/react-native-skia';
 
 const RippleEffect = () => {
+  const width = 256;
+  const height = 256;
+  const r = width * 0.33;
   return (
     <View style={styles.container}>
-      <Text>Ripple Effect</Text>
+      <Canvas style={{width, height}}>
+        <Group blendMode="multiply">
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={width - r} cy={r} r={r} color="magenta" />
+          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+        </Group>
+      </Canvas>
     </View>
   );
 };
